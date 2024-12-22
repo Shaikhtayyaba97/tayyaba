996d4067fc36d54971eb912eaab5fd26676bc75c
tayyaba <tayyabashahbaz2020@gmail.com>
2024-12-22 23:52:37 +0500

    little addition

--- src/app/women/facepowder/[Id]/page.tsx
+++ src/app/women/facepowder/[Id]/page.tsx
@@ -1,4 +1,4 @@
-import { Facepowderdata } from "../../../../../Data/data";
+import { Facepowderdata } from "../../../../../Data/data"
 import Image from "next/image"
 interface Paramsin {
     params: {
@@ -6,14 +6,13 @@ interface Paramsin {
     }
 }
 const page = ({params} : Paramsin) => {
-    const tonum = Number(params.id);
-    const filter = Facepowderdata.find((items) => items.id === tonum);
-    
+    const tonum = Number(params.id)
+    const filter = Facepowderdata.find((items) => items.id === tonum)
 
   return (
     <div className=" text-purple-600 flex flex-col justify-center items-center m-4 gap-3">
-      <Image src={filter?.img || './img' } alt='img' height={400} width={400}/>
-        
+      <Image src={filter?.img || './img' } alt='img' height={100} width={100}/>
+       
         <p>{filter?.title}</p>
         <h1>{filter?.price}</h1>
         <button className='bg-purple-200 text-purple-950'>Add to card</button> 
