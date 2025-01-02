ea9a6533e019940ef24e6c51a673a0f437cbc3fd
tayyaba <tayyabashahbaz2020@gmail.com>
2025-01-02 19:23:43 +0500

    added little changes in color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -41,7 +41,7 @@ const Button: React.FC<ButtonProps> = ({
       {/* Decrement Button */}
       <button
         onClick={decrementQuantity}
-        className="px-3 py-2 bg-gray-200 rounded hover:bg-gray-300"
+        className="px-3 py-2 bg-gray-600 rounded hover:bg-gray-300"
         aria-label="Decrement Quantity"
       >
         <AiOutlineMinus />
@@ -53,7 +53,7 @@ const Button: React.FC<ButtonProps> = ({
       {/* Increment Button */}
       <button
         onClick={incrementQuantity}
-        className="px-3 py-2 bg-gray-200 rounded hover:bg-gray-300"
+        className="px-3 py-2 bg-gray-600 rounded hover:bg-gray-300"
         aria-label="Increment Quantity"
       >
         <AiOutlinePlus />
